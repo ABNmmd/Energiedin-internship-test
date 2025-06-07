461e439f0e12fe4c328b7cb068a8097e52f4425b
ABNmmd <moha.ww.ha@gmail.com>
2025-06-07 02:20:29 +0100

    Add missing components to Home layout for complete page structure

--- energiedin-internship-test/src/app/page.js
+++ energiedin-internship-test/src/app/page.js
@@ -1,5 +1,13 @@
 import Header from "@/components/Header";
 import Banner from "@/components/Banner";
+import AboutUs from "@/components/AboutUs";
+import Process from "@/components/Process";
+import Prerequisites from "@/components/Prerequisites";
+import Products from "@/components/Products";
+import StoreLocation from "@/components/StoreLocation";
+import Contact from "@/components/Contact";
+import ContactForm from "@/components/ContactForm";
+import CompanyInfo from "@/components/CompanyInfo";
 import Image from "next/image";
 
 export default function Home() {
@@ -8,6 +16,14 @@ export default function Home() {
       <Header />
       <main className="">
         <Banner />
+        <AboutUs />
+        <Process />
+        <Prerequisites />
+        <Products />
+        <StoreLocation />
+        <Contact />
+        <ContactForm />
+        <CompanyInfo />
       </main>
     </div>
   );
