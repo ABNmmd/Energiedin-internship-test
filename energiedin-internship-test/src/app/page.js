8e031fe415d492a366ac3c74031da94dcdb59235
ABNmmd <moha.ww.ha@gmail.com>
2025-06-08 01:43:28 +0100

    Add ContactServices, ExperienceSection, Testimonials, and Footer components to Home page layout

--- energiedin-internship-test/src/app/page.js
+++ energiedin-internship-test/src/app/page.js
@@ -8,6 +8,10 @@ import StoreLocation from "@/components/StoreLocation";
 import Contact from "@/components/Contact";
 import ContactForm from "@/components/ContactForm";
 import CompanyInfo from "@/components/CompanyInfo";
+import ContactServices from "@/components/ContactServices";
+import ExperienceSection from "@/components/ExperienceSection";
+import Testimonials from "@/components/Testimonials";
+import Footer from "@/components/Footer";
 import Image from "next/image";
 
 export default function Home() {
@@ -24,7 +28,11 @@ export default function Home() {
         <Contact />
         <ContactForm />
         <CompanyInfo />
+        <ContactServices />
+        <ExperienceSection />
+        <Testimonials />
       </main>
+      <Footer />
     </div>
   );
 }
